63ab967d6187bd6527ba43ef031c6437be282b00
Renata Pausini <rpatricia@outlook.com>
2022-11-19 18:04:16 -0300

    Poke-api criada.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-const offeset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offeset=${0}&limit=${limit}`;
+
+
 
 function convertPokemonToLi(pokemon) {
   return `
@@ -20,10 +19,8 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById("pokemonList");
   
-fetch(url)
-  .then((response) => response.json())
-  .then((jsonBody) => jsonBody.results)
-  .then((pokemons) => {
+
+  pokeApi.getPokemons().then((pokemons) => {
     for (let i = 0; i < pokemons.length; i++) {
       const pokemon = pokemons[i];    
       pokemonList.innerHTML += convertPokemonToLi(pokemon);
