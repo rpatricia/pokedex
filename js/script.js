c7611c58bf10e0452f232f5135737e678d7667f9
Renata Pausini <rpatricia@outlook.com>
2022-11-19 13:06:11 -0300

    JavaScript inicializado :sparkles:

--- js/script.js
+++ js/script.js
@@ -2,16 +2,31 @@ const offeset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offeset=${0}&limit=${limit}`;
 
+function convertPokemonToLi(pokemon) {
+  return `
+  <li class="pokemon">
+    <span class="number">#001</span>
+    <span class="name"> ${pokemon.name}</span>
+    <div class="detail">
+      <ol class="types">
+        <li class="type">grass</li>
+        <li class="type">poison</li>
+      </ol>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg " alt=${pokemon.name}>
+    </div>
+  </li>
+  `;
+}
+
+const pokemonList = document.getElementById("pokemonList");
+  
 fetch(url)
- .then((response)=>{
-    //return console.log(response)
-    return response.json() //promessa de um json dentro body
-  }).then((jsonBody)=>{
-    return console.log(jsonBody) //json body convertido
+  .then((response) => response.json())
+  .then((jsonBody) => jsonBody.results)
+  .then((pokemons) => {
+    for (let i = 0; i < pokemons.length; i++) {
+      const pokemon = pokemons[i];    
+      pokemonList.innerHTML += convertPokemonToLi(pokemon);
+    }
   })
- .catch((error)=>{
-    return console.error(error)
-  })
- .finally((error)=>{
-    return console.log("Requisição concluída!")
-  })
+  .catch((error) => console.error(error));
