8f3537d401d698a51753ed7785600f9e843661a3
Renata Pausini <rpatricia@outlook.com>
2022-11-20 19:26:38 -0300

    Map e promisse.

--- js/poke-api.js
+++ js/poke-api.js
@@ -2,8 +2,17 @@ const pokeApi = {};
 
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-  return  fetch(url)
+  return fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.error(error));
+    .then((p0kemons) => pokemons.map((pokemon) => fetch(pokemon.url).json()));
 };
+
+Promise.all([
+  fetch("https://pokeapi.co/api/v2/pokemon/1"),
+  fetch("https://pokeapi.co/api/v2/pokemon/2"),
+  fetch("https://pokeapi.co/api/v2/pokemon/3"),
+  fetch("https://pokeapi.co/api/v2/pokemon/4"),
+]).then((results) => {
+  console.log(results);
+});
