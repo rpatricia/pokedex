2c4279be885d5c3b94918d44f9da5daa1c00cd1a
Renata Pausini <rpatricia@outlook.com>
2022-11-20 12:12:22 -0300

    Reduced map

--- js/script.js
+++ js/script.js
@@ -20,7 +20,8 @@ function convertPokemonToLi(pokemon) {
 const pokemonList = document.getElementById("pokemonList");
 
 pokeApi.getPokemons().then((pokemons = []) => {
-  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+  const newHTML = pokemons.map(convertPokemonToLi).join('')
+  pokemonList.innerHTML += newHTML
 })
 
   
