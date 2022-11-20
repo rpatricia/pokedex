3e305ec7c4ecc66f9691dbba7761124ef3521c8d
Renata Pausini <rpatricia@outlook.com>
2022-11-20 20:48:11 -0300

    continuation of the api

--- js/poke-api.js
+++ js/poke-api.js
@@ -9,8 +9,8 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
   return fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail)
-    .then((detailRequests) => Promise.all(dtailRequests))
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
     .then((pokemonsDetails) => {
       debugger;
       console.log(pokemonsDetails);
